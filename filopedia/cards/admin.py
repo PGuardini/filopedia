49e2bee16e9da88250a51562eb221c1f3e7b7a24
Pedro Gabriel Gabiatti Guardini <pgabiattiguardini@gmail.com>
2026-02-11 21:11:33 -0300

    Implementação banco de dados deployment

--- filopedia/cards/admin.py
+++ filopedia/cards/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from cards.models import Filosofo
+
+class ListaFilosofos(admin.ModelAdmin):
+    list_display = ("id", "nome", "referencias", "publicado")
+    list_display_links = ("id", "nome",)
+    search_fields = ("nome", "slug",)
+    #list_filter = ("nome",)
+    list_editable = ("publicado",)
+    list_per_page = 20
 # Register your models here.
+admin.site.register(Filosofo, ListaFilosofos)
--- filopedia/cards/models.py
+++ filopedia/cards/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# from data_retriever import get_all_data
+# from constants import DATA_PATH
+# import json
+
 
 # Create your models here.
+class Filosofo(models.Model):
+    nome = models.CharField(max_length=100, null=False)
+    biografia = models.TextField(null=False, blank=False)
+    imagem = models.CharField(null=False, blank=False)
+    referencias = models.CharField(null=False, blank=False)
+    slug = models.CharField(max_length=120, null=False, blank=False)
+    publicado = models.BooleanField(default=True)
